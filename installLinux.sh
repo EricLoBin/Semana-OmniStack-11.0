d3d7eabd862afaf934995cb855c28c1380365a40
EricLoBin <ekilobin@gmail.com>
2020-03-24 14:42:50 -0300

    [ADD] Auto install node modules Linux and Windows

---
+++ installLinux.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd aulas
+cd backend
+npm install
+cd ../frontend
+npm install
---
+++ installWindows.bat
@@ -0,0 +1,12 @@
+@echo off
+cd aulas
+cd backend
+@echo on
+npm install
+@echo off
+cd..
+cd frontend
+@echo on
+npm install
+@echo off
+pause

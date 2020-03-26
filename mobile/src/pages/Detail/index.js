d6adcef2a88641fb68a51e8b7251072a0f547a23
EricLoBin <ekilobin@gmail.com>
2020-03-26 14:23:00 -0300

    [UPDATE] Aula 4

--- mobile/src/pages/Detail/index.js
+++ mobile/src/pages/Detail/index.js
@@ -18,7 +18,31 @@ export default function Detail() {
             </View>
 
             <View style={styles.incident}>
-                
+                <Text style={[styles.incidentProperty, {marginTop: 0}]}>ONG:</Text>
+                <Text style={styles.incidentValue}>APAD</Text>
+
+                <Text style={styles.incidentProperty}>CASO:</Text>
+                <Text style={styles.incidentValue}>Cadelinha atropelada</Text>
+                    
+                <Text style={styles.incidentProperty}>VALOR:</Text>
+                <Text style={styles.incidentValue}>R$ 120,00</Text>
+            </View>
+
+            <View style={styles.contactBox}>
+                <Text style={styles.heroTitle} >Salve o dia!</Text>
+                <Text style={styles.heroTitle} >Seja o herói deste caso.</Text>
+
+                <Text style={styles.heroDescription} >Entre em contato:</Text>
+
+                <View style={styles.actions}>
+                    <TouchableOpacity styles={styles.action} onPress={() => {}}>
+                        <Text style={styles.actionText}>WhatsApp</Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity styles={styles.action} onPress={() => {}}>
+                        <Text style={styles.actionText}>E-mail</Text>
+                    </TouchableOpacity>
+                </View>
             </View>
         </View>
     );

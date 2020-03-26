ce82d0722878509351981388fd814a40c9615a7b
EricLoBin <ekilobin@gmail.com>
2020-03-25 23:14:43 -0300

    [UPDATE] Aula 3

--- aulas/frontend/src/pages/Profile/index.js
+++ aulas/frontend/src/pages/Profile/index.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { FiPower, FiTrash2 } from 'react-icons/fi';
 
 import './styles.css';
@@ -9,6 +9,8 @@ import api from '../../services/api';
 
 export default function Profile() {
     const [incidents, setIncidents] = useState([]);
+
+    const history = useHistory();
 
     const ongId = localStorage.getItem('ongId');
     const ongName = localStorage.getItem('ongName');
@@ -25,7 +27,7 @@ export default function Profile() {
 
     async function handleDeleteIncident(id) {
         try {
-            await api.delete(`incidents/${id}`,{
+            await api.delete(`incident/${id}`,{
                 headers:{
                     Authorization: ongId,
                 }
@@ -36,6 +38,12 @@ export default function Profile() {
         }
     };
 
+    function handleLogout() {
+        localStorage.clear();
+
+        history.push('/');
+    }
+
     return (
         <div className="profile-container">
             <header>
@@ -43,7 +51,7 @@ export default function Profile() {
                 <span>Bem vinda, {ongName}</span>
 
                 <Link className="button" to="/incidents/new">Cadastrar novo caso</Link>
-                <button type="button">
+                <button onClick={handleLogout} type="button">
                     <FiPower size={18} color="#e02041" />
                 </button>
             </header>
